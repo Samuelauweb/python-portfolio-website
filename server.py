f77ba850f57e4abeb9ae6e5d76f316e4c3e5eb68
Samuel <samuel.cflau@gmail.com>
2022-03-31 12:47:48 +0800

    Building A Portfolio 1: Mashup Templates

--- server.py
+++ server.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 print(__name__)
 
 @app.route("/")
-def hello_world():
+def my_home():
     return render_template('index.html')
+
+@app.route("/works")
+def works():
+    return render_template('works.html')
 
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/components")
+def components():
+    return render_template('components.html')
   
-@app.route("/blog")
-def blog():
-    return "<p>This is my first blog post</p>"
+@app.route("/<username>/<int:post_id>")
+def blog(username = None, post_id = None):
+    return render_template('index.html', name=username, post_id=post_id)
   
 @app.route("/blog/2022/dogs")
 def blog2():
